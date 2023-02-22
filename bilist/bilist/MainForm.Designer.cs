894e3267fea7c210c4aa91c2186639bd0a865c85
Asterecho <2548299040@qq.com>
2023-02-23 01:43:55 +0800

    +

---
+++ bilist/bilist/MainForm.Designer.cs
@@ -0,0 +1,246 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： ifwz
+ * 日期: 2023/2/22
+ * 时间: 23:12
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+namespace bilist
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private Sunny.UI.UIButton uiButton1;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private Sunny.UI.UIButton uiButton2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private Sunny.UI.UIButton uiButton3;
+		private Sunny.UI.UIButton uiButton4;
+		private Sunny.UI.UIButton uiButton5;
+		private System.Windows.Forms.ToolTip toolTip1;
+		private Sunny.UI.UIButton uiButton6;
+		private Sunny.UI.UIButton uiButton7;
+		private Sunny.UI.UIButton uiButton8;
+		private Sunny.UI.UIButton uiButton9;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.uiButton1 = new Sunny.UI.UIButton();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.uiButton2 = new Sunny.UI.UIButton();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.uiButton3 = new Sunny.UI.UIButton();
+			this.uiButton4 = new Sunny.UI.UIButton();
+			this.uiButton5 = new Sunny.UI.UIButton();
+			this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+			this.uiButton8 = new Sunny.UI.UIButton();
+			this.uiButton9 = new Sunny.UI.UIButton();
+			this.uiButton6 = new Sunny.UI.UIButton();
+			this.uiButton7 = new Sunny.UI.UIButton();
+			this.SuspendLayout();
+			// 
+			// uiButton1
+			// 
+			this.uiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.uiButton1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton1.Location = new System.Drawing.Point(103, 338);
+			this.uiButton1.MinimumSize = new System.Drawing.Size(1, 1);
+			this.uiButton1.Name = "uiButton1";
+			this.uiButton1.Size = new System.Drawing.Size(47, 41);
+			this.uiButton1.TabIndex = 0;
+			this.uiButton1.Text = "空间";
+			this.uiButton1.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+			this.uiButton1.Click += new System.EventHandler(this.UiButton1Click);
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.Font = new System.Drawing.Font("微软雅黑", 15F);
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Location = new System.Drawing.Point(3, 38);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(432, 294);
+			this.checkedListBox1.TabIndex = 1;
+			// 
+			// uiButton2
+			// 
+			this.uiButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.uiButton2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton2.Location = new System.Drawing.Point(360, 338);
+			this.uiButton2.MinimumSize = new System.Drawing.Size(1, 1);
+			this.uiButton2.Name = "uiButton2";
+			this.uiButton2.Size = new System.Drawing.Size(69, 41);
+			this.uiButton2.TabIndex = 2;
+			this.uiButton2.Text = "处理";
+			this.uiButton2.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+			this.uiButton2.Click += new System.EventHandler(this.UiButton2Click);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Font = new System.Drawing.Font("微软雅黑", 15F);
+			this.checkBox1.Location = new System.Drawing.Point(9, 338);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(73, 32);
+			this.checkBox1.TabIndex = 3;
+			this.checkBox1.Text = "全选";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1CheckedChanged);
+			// 
+			// uiButton3
+			// 
+			this.uiButton3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.uiButton3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton3.Location = new System.Drawing.Point(158, 338);
+			this.uiButton3.MinimumSize = new System.Drawing.Size(1, 1);
+			this.uiButton3.Name = "uiButton3";
+			this.uiButton3.Size = new System.Drawing.Size(69, 41);
+			this.uiButton3.TabIndex = 4;
+			this.uiButton3.Text = "收藏夹";
+			this.uiButton3.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton3.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+			this.uiButton3.Click += new System.EventHandler(this.UiButton3Click);
+			// 
+			// uiButton4
+			// 
+			this.uiButton4.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.uiButton4.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton4.Location = new System.Drawing.Point(5, 366);
+			this.uiButton4.MinimumSize = new System.Drawing.Size(1, 1);
+			this.uiButton4.Name = "uiButton4";
+			this.uiButton4.Size = new System.Drawing.Size(20, 20);
+			this.uiButton4.TabIndex = 5;
+			this.uiButton4.Text = "①";
+			this.uiButton4.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton4.TipsText = "uid";
+			this.toolTip1.SetToolTip(this.uiButton4, "uid");
+			this.uiButton4.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+			this.uiButton4.Click += new System.EventHandler(this.UiButton4Click);
+			// 
+			// uiButton5
+			// 
+			this.uiButton5.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.uiButton5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton5.Location = new System.Drawing.Point(29, 366);
+			this.uiButton5.MinimumSize = new System.Drawing.Size(1, 1);
+			this.uiButton5.Name = "uiButton5";
+			this.uiButton5.Size = new System.Drawing.Size(20, 20);
+			this.uiButton5.TabIndex = 6;
+			this.uiButton5.Text = "②";
+			this.uiButton5.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton5.TipsText = "media_id";
+			this.toolTip1.SetToolTip(this.uiButton5, "media_id");
+			this.uiButton5.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+			this.uiButton5.Click += new System.EventHandler(this.UiButton5Click);
+			// 
+			// uiButton8
+			// 
+			this.uiButton8.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.uiButton8.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton8.Location = new System.Drawing.Point(54, 366);
+			this.uiButton8.MinimumSize = new System.Drawing.Size(1, 1);
+			this.uiButton8.Name = "uiButton8";
+			this.uiButton8.Size = new System.Drawing.Size(20, 20);
+			this.uiButton8.TabIndex = 9;
+			this.uiButton8.Text = "③";
+			this.uiButton8.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton8.TipsText = "media_id";
+			this.toolTip1.SetToolTip(this.uiButton8, "media_id");
+			this.uiButton8.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+			this.uiButton8.Click += new System.EventHandler(this.UiButton8Click);
+			// 
+			// uiButton9
+			// 
+			this.uiButton9.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.uiButton9.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton9.Location = new System.Drawing.Point(79, 366);
+			this.uiButton9.MinimumSize = new System.Drawing.Size(1, 1);
+			this.uiButton9.Name = "uiButton9";
+			this.uiButton9.Size = new System.Drawing.Size(20, 20);
+			this.uiButton9.TabIndex = 10;
+			this.uiButton9.Text = "④";
+			this.uiButton9.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton9.TipsText = "media_id";
+			this.toolTip1.SetToolTip(this.uiButton9, "media_id");
+			this.uiButton9.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+			this.uiButton9.Click += new System.EventHandler(this.UiButton9Click);
+			// 
+			// uiButton6
+			// 
+			this.uiButton6.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.uiButton6.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton6.Location = new System.Drawing.Point(247, 338);
+			this.uiButton6.MinimumSize = new System.Drawing.Size(1, 1);
+			this.uiButton6.Name = "uiButton6";
+			this.uiButton6.Size = new System.Drawing.Size(45, 41);
+			this.uiButton6.TabIndex = 7;
+			this.uiButton6.Text = "历史";
+			this.uiButton6.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton6.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+			this.uiButton6.Click += new System.EventHandler(this.UiButton6Click);
+			// 
+			// uiButton7
+			// 
+			this.uiButton7.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.uiButton7.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton7.Location = new System.Drawing.Point(298, 338);
+			this.uiButton7.MinimumSize = new System.Drawing.Size(1, 1);
+			this.uiButton7.Name = "uiButton7";
+			this.uiButton7.Size = new System.Drawing.Size(45, 41);
+			this.uiButton7.TabIndex = 8;
+			this.uiButton7.Text = "稍后";
+			this.uiButton7.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.uiButton7.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+			this.uiButton7.Click += new System.EventHandler(this.UiButton7Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+			this.ClientSize = new System.Drawing.Size(438, 393);
+			this.Controls.Add(this.uiButton9);
+			this.Controls.Add(this.uiButton8);
+			this.Controls.Add(this.uiButton7);
+			this.Controls.Add(this.uiButton6);
+			this.Controls.Add(this.uiButton5);
+			this.Controls.Add(this.uiButton4);
+			this.Controls.Add(this.uiButton3);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.uiButton2);
+			this.Controls.Add(this.checkedListBox1);
+			this.Controls.Add(this.uiButton1);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "bilist";
+			this.TopMost = true;
+			this.ZoomScaleRect = new System.Drawing.Rectangle(15, 15, 800, 480);
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ bilist/bilist/MainForm.cs
@@ -0,0 +1,146 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： ifwz
+ * 日期: 2023/2/22
+ * 时间: 23:12
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+using LitJson;
+using Sunny.UI;
+
+namespace bilist
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : UIForm
+	{
+		public MainForm()
+		{
+			//https://api.bilibili.com/x/space/wbi/arc/search?mid=1278081874   获取up空间视频
+			//
+			//https://api.bilibili.com/x/v3/fav/resource/list?media_id=87317442&pn=1&ps=20 收藏夹
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	 
+		void UiButton1Click(object sender, EventArgs e)
+		{
+			string rawjson=Clipboard.GetText();
+			JsonData json=JsonMapper.ToObject(rawjson);  //https://blog.csdn.net/DoyoFish/article/details/81976181
+			if ((int)json["code"]==0) {
+				JsonData data=json["data"]["list"]["vlist"];
+				
+				for (int i = 0; i < data.Count; i++) {
+					checkedListBox1.Items.Add(data[i]["title"].ToString()+"#"+data[i]["bvid"].ToString());
+				}
+				
+				
+			}
+		}
+		void UiButton2Click(object sender, EventArgs e)
+		{
+			string tt="@echo off\n";
+				for (int i = 0; i < checkedListBox1.Items.Count; i++)
+			{
+			  if (checkedListBox1.GetItemChecked(i))
+			  {
+			  	string t=checkedListBox1.GetItemText(checkedListBox1.Items[i]);
+			  	string title=t.Split('#')[0];
+			  	string bvid=t.Split('#')[1];
+			  	tt+="mkdir \""+title+"\""+"\n"+"lux -c cookies.txt -C -o \""+title+"\""+" -O \""+t+"\" \""+"https://www.bilibili.com/video/"+bvid+"\""+"\n";
+			  }
+			}
+				 
+				Clipboard.SetText(tt);
+		}
+		void CheckBox1CheckedChanged(object sender, EventArgs e)
+		{
+			if(checkBox1.Checked) 
+			{
+			     for (int j = 0; j < checkedListBox1.Items.Count; j++) 
+			        checkedListBox1.SetItemChecked(j, true); 
+			}
+			else
+			{
+			for (int j =0; j < checkedListBox1.Items.Count; j++) 
+			   checkedListBox1.SetItemChecked(j, false);
+			}
+		}
+		void UiButton4Click(object sender, EventArgs e)
+		{
+			Process.Start("https://api.bilibili.com/x/space/wbi/arc/search?mid="+Clipboard.GetText());
+		}
+		void UiButton5Click(object sender, EventArgs e)
+		{
+			Process.Start("https://api.bilibili.com/x/v3/fav/resource/list?media_id="+Clipboard.GetText()+"&pn=1&ps=20");
+		}
+		void UiButton3Click(object sender, EventArgs e)
+		{
+			string rawjson=Clipboard.GetText();
+			JsonData json=JsonMapper.ToObject(rawjson);  //https://blog.csdn.net/DoyoFish/article/details/81976181
+			if ((int)json["code"]==0) {
+				JsonData data=json["data"]["medias"];
+				
+				for (int i = 0; i < data.Count; i++) {
+					checkedListBox1.Items.Add(data[i]["title"].ToString()+"#"+data[i]["bvid"].ToString());
+				}
+				
+				
+			}
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+	
+		}
+		void UiButton8Click(object sender, EventArgs e)
+		{
+			Process.Start("https://api.bilibili.com/x/web-interface/history/cursor");
+		}
+		void UiButton9Click(object sender, EventArgs e)
+		{
+			Process.Start("https://api.bilibili.com/x/v2/history/toview/web?jsonp=jsonp");
+		}
+		void UiButton6Click(object sender, EventArgs e)
+		{
+			string rawjson=Clipboard.GetText();
+			JsonData json=JsonMapper.ToObject(rawjson);  //https://blog.csdn.net/DoyoFish/article/details/81976181
+			if ((int)json["code"]==0) {
+				JsonData data=json["data"]["list"];
+				
+				for (int i = 0; i < data.Count; i++) {
+					checkedListBox1.Items.Add(data[i]["title"].ToString()+"#"+data[i]["history"]["bvid"].ToString());
+				}
+				
+				
+			}
+		}
+		void UiButton7Click(object sender, EventArgs e)
+		{
+			string rawjson=Clipboard.GetText();
+			JsonData json=JsonMapper.ToObject(rawjson);  //https://blog.csdn.net/DoyoFish/article/details/81976181
+			if ((int)json["code"]==0) {
+				JsonData data=json["data"]["list"];
+				
+				for (int i = 0; i < data.Count; i++) {
+					checkedListBox1.Items.Add(data[i]["title"].ToString()+"#"+data[i]["bvid"].ToString());
+				}
+				
+				
+			}
+		}
+	}
+}
